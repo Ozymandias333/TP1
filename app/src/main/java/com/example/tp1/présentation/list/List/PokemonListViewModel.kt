f21f35b9e5bfa95a2bed66d37f0df1626e5eb21a
Ozymandias333 <lorenzolamberti3@gmail.com>
2021-05-23 22:58:53 +0200

    Almost finished

--- app/src/main/java/com/example/tp1/présentation/list/List/PokemonListViewModel.kt
+++ app/src/main/java/com/example/tp1/présentation/list/List/PokemonListViewModel.kt
@@ -11,23 +11,26 @@ import retrofit2.Response
 
 class PokemonListViewModel : ViewModel(){
 
-    val pokeList : MutableLiveData<List<Pokemon>> = MutableLiveData()
+    val pokeList : MutableLiveData<PokemonModel> = MutableLiveData()
 
     init {
         callApi()
     }
 
     private fun callApi() {
+        pokeList.value = PokemonLoader
         Singletons.pokemonAPI.getPokemonList()?.enqueue(object : Callback<PokemonListResponse> {
             override fun onFailure(call: Call<PokemonListResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                pokeList.value = PokemonError
+
             }
 
             override fun onResponse(call: Call<PokemonListResponse>, response: Response<PokemonListResponse>) {
                 if(response.isSuccessful && response.body() != null) {
                     val pokemonResponse= response.body()!!
-                    pokeList.value = pokemonResponse.results
-  //                  adapter.updateList(pokemonResponse.results)
+                    pokeList.value = PokemonSucces(pokemonResponse.results)
+                } else {
+                    pokeList.value = PokemonError
                 }
             }
 
