38dcf847082953ad1bfd9c156508a108c1d12b96
Ozymandias333 <lorenzolamberti3@gmail.com>
2021-05-23 21:42:30 +0200

    Images added

--- app/src/main/java/com/example/tp1/présentation/list/List/PokemonAdapter.kt
+++ app/src/main/java/com/example/tp1/présentation/list/List/PokemonAdapter.kt
@@ -3,8 +3,10 @@ package com.example.tp1.présentation.list.List
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.tp1.R
 
 class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int)->Unit)? = null) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
@@ -18,10 +20,12 @@ class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int)->U
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val textView: TextView
+        val imageView: ImageView
 
         init {
             // Define click listener for the ViewHolder's View.
-             textView = view.findViewById(R.id.pokemon_name)
+            textView = view.findViewById(R.id.pokemon_name)
+            imageView = view.findViewById(R.id.pokemon_img)
         }
     }
 
@@ -48,6 +52,12 @@ class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int)->U
         viewHolder.itemView.setOnClickListener {
             listener?.invoke(position)
         }
+
+        Glide
+            .with(viewHolder.itemView.context)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${position +1 }.png")
+            .centerCrop()
+            .into(viewHolder.imageView)
     }
 
     // Return the size of your dataset (invoked by the layout manager)
